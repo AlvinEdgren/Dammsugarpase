0b346a18fe8490a9dacefaaf3f7efbb2fb7d34ce
AlvinEdgren <alvin.anders.edgren@gmail.com>
2026-04-16 20:15:41 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, fontFamily: "Arial, sans-serif" }}>
-      <h1>Dammsugarpase.com</h1>
-      <p>Sidan är live nu.</p>
-    </main>
-  );
-}
